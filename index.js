4bef98d5782be4c69be8ea34e2c3c3fcc1ba62f2
Ammar-coder87 <ammar.kolko87@gmail.com>
2023-10-07 23:26:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(string = "roller-skate") {
+return `This Saturday, I want to ${string}!`
+}
+console.log(saturdayFun());
+
+console.log(saturdayFun("bathe my dog"));
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+function mondayWork(string = "go to the office"){
+    return `This Monday, I will ${string}.`
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+
+function wrapAdjective( visualFlair = "*" ) {
+    return function ( nameOfAdj = "a hard worker" ) {
+        return `You are ${visualFlair}${nameOfAdj}${visualFlair}!`;
+    }
+    
+}
+console.log(wrapAdjective()());
+console.log(wrapAdjective("||")("a dedicated programmer"));
